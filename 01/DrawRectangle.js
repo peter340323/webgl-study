f390a9c16c94314a26615f486e7c625f332e4fd0
peter340323 <46309589+peter340323@users.noreply.github.com>
2019-01-06 23:50:39 +0800

    Add files via upload

---
+++ 01/DrawRectangle.js
@@ -0,0 +1,11 @@
+//DrawRectangle.js
+function main(){
+	var canvas=document.getElementById('example');
+	if(!canvas){
+		console.log('Failed to retieve the<canvas>element');
+		return;
+}
+var ctx=canvas.getContext('2d');
+ctx.fillStyle='rgba(0,0,255,1.0)';
+ctx.fillRect(120,10,150,150);
+}
---
+++ 01/readme.txt
@@ -0,0 +1,5 @@
+＃  webgl-study
+学习共勉
+document.getElementById（“example”）// exanmple是标记，上文中的id，getElementById字面意思是得到元素通过id即获取画布
+canvas.getContext（ '2D'）//获取绘图上下文
+RGBA（R，G，B，A）//一个代表透明度
